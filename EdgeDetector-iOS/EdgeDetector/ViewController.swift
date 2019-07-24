cb2502c0c929a92c50aa8746a52922d4471ce14e
Greg <grzegorz.surma@icloud.com>
2019-07-24 13:03:46 +0200

    initial commit

---
+++ EdgeDetector-iOS/EdgeDetector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EdgeDetector
+//
+//  Created by Greg on 24/07/2019.
+//  Copyright © 2019 GS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
